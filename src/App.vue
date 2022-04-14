eb67038d60fd030856fdfe65309147bc3fa04d96
Michael Miller <mullerg4michael@gmail.com>
2022-04-14 09:18:51 -0300

    SideBar Example

--- src/App.vue
+++ src/App.vue
@@ -3,71 +3,18 @@
     <div id="view" :class="[{ collapsed: collapsed }]">
       <router-view />
     </div>
-    <sidebar-menu
-      class="sidebar"
-      :menu="menu"
-      :collapsed="collapsed"
-      hideToggle
-      @item-click="onItemClick"
-      @collapse="onCollapse"
-    />
+    <SideBar/>
   </div>
 </template>
 
 <script>
+
+import SideBar from '@/components/SideBar.vue'
+
 export default {
   name: "App",
-  data() {
-    return {
-      menu: [
-        {
-          header: true,
-          title: "S-Partner",
-        },
-        {
-          href: "/cursos",
-          title: "Cursos",
-          icon: "fa fa-list-ul",
-          child: [
-            {
-              href: "/cursos/curso-1",
-              title: "Curso 1",
-              icon: "fa fa-file-alt",
-              child: [
-                {
-                  href: "/cursos/curso-1/contents/1",
-                  title: "Vídeo 1",
-                  icon: "fa fa-file",
-                },
-                {
-                  href: "/cursos/curso-1/contents/2",
-                  title: "Texto 1",
-                  icon: "fa fa-file",
-                },
-              ],
-            },
-            {
-              href: "/cursos/curso-2",
-              title: "Curso 2",
-              icon: "fa fa-file-alt",
-              child: [
-                {
-                  href: "/cursos/curso-2/contents/1",
-                  title: "Vídeo 1",
-                  icon: "fa fa-file",
-                },
-                {
-                  href: "/cursos/curso-2/contents/2",
-                  title: "Problema 3",
-                  icon: "fa fa-file",
-                },
-              ],
-            },
-          ],
-        },
-      ],
-      collapsed: true,
-    };
+  components: {
+    SideBar
   }
 };
 </script>
@@ -78,14 +25,5 @@ export default {
 }
 #view.collapsed {
   padding-left: 50px;
-}
-
-.sidebar.v-sidebar-menu .vsm-arrow:after {
-  content: "\f105";
-  font-family: "FontAwesome";
-}
-.sidebar.v-sidebar-menu .collapse-btn:after {
-  content: "\f07e";
-  font-family: "FontAwesome";
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,78 @@
+<template>
+  <sidebar-menu
+      class="sidebar"
+      :menu="menu"
+      :collapsed="collapsed"
+      hideToggle
+      @item-click="onItemClick"
+      @collapse="onCollapse"
+    />
+</template>
+
+<script>
+export default {
+data() {
+    return {
+      menu: [
+        {
+          header: true,
+          title: "S-Partner",
+        },
+        {
+          href: "/cursos",
+          title: "Cursos",
+          icon: "fa fa-list-ul",
+          child: [
+            {
+              href: "/cursos/curso-1",
+              title: "Curso 1",
+              icon: "fa fa-file-alt",
+              child: [
+                {
+                  href: "/cursos/curso-1/contents/1",
+                  title: "Vídeo 1",
+                  icon: "fa fa-file",
+                },
+                {
+                  href: "/cursos/curso-1/contents/2",
+                  title: "Texto 1",
+                  icon: "fa fa-file",
+                },
+              ],
+            },
+            {
+              href: "/cursos/curso-2",
+              title: "Curso 2",
+              icon: "fa fa-file-alt",
+              child: [
+                {
+                  href: "/cursos/curso-2/contents/1",
+                  title: "Vídeo 1",
+                  icon: "fa fa-file",
+                },
+                {
+                  href: "/cursos/curso-2/contents/2",
+                  title: "Problema 3",
+                  icon: "fa fa-file",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      collapsed: true,
+    };
+    }
+}
+</script>
+
+<style>
+.sidebar.v-sidebar-menu .vsm-arrow:after {
+  content: "\f105";
+  font-family: "FontAwesome";
+}
+.sidebar.v-sidebar-menu .collapse-btn:after {
+  content: "\f07e";
+  font-family: "FontAwesome";
+}
+</style>
